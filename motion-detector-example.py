075a4c575973d80534c8899c04280e6f7e09006c
Nitis Monburinon <boonitis@gmail.com>
2018-07-25 11:47:40 +0900

    Init

---
+++ motion-detector-example.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import time
+import RPi.GPIO as GPIO
+
+# Setup GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN, GPIO.PUD_DOWN)
+GPIO.setup(10, GPIO.OUT)
+
+# Main code goes below here
+try:
+    while True:
+
+	# Detect motion via PIR Sensor
+        detect = GPIO.input(4)
+
+        # When PIR sensor detect motion
+        if(detect == 1):
+            print("Motion Detected")
+            # Blink LED
+            GPIO.output(10,GPIO.HIGH)
+            print("LED ON")
+            # Wait 1 second
+            time.sleep(1)
+            GPIO.output(10,GPIO.LOW)
+            print("LED OFF")
+        # Wait for further motion
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
