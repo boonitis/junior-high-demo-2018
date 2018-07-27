c622b80ef1f3ee342e0f2739c16ebe5ec2a3a3a2
Nitis Monburinon <boonitis@gmail.com>
2018-07-27 13:49:59 +0900

    Fix

--- send_data.py
+++ send_data.py
@@ -1,10 +1,7 @@
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
+import requests
 
 def send(name):
-    url = 'https://192.168.11.35' # Set destination URL here
-    post_fields = {'text': name}     # Set POST fields here
+    payload = {'text': name}
 
-    request = Request(url, urlencode(post_fields).encode())
-    json = urlopen(request).read().decode()
-    print(json)
+    r = request.post("http://192.168.11.35/", data=payload)
+    print(r.text)
