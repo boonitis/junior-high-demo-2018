e9885b6c92025534956882e6fe0379808b03dbbf
Nitis Monburinon <boonitis@gmail.com>
2018-07-27 13:50:48 +0900

    Fix

--- send_data.py
+++ send_data.py
@@ -3,5 +3,5 @@ import requests
 def send(name):
     payload = {'text': name}
 
-    r = request.post("http://192.168.11.35/", data=payload)
+    r = requests.post("http://192.168.11.35/", data=payload)
     print(r.text)
