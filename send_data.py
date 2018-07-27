83b7ae3280bbaf14375925028ecc111cda8f8770
Nitis Monburinon <boonitis@gmail.com>
2018-07-27 13:42:08 +0900

    Add send data functionality

--- send_data.py
+++ send_data.py
@@ -1,2 +1,10 @@
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
 def send(name):
-    print('Hello world:' + name)
+    url = 'https://192.168.11.35' # Set destination URL here
+    post_fields = {'text': name}     # Set POST fields here
+
+    request = Request(url, urlencode(post_fields).encode())
+    json = urlopen(request).read().decode()
+    print(json)
