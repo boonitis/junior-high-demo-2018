8ce81dad52d6efc1f71aad672b97dbde91d6edf5
Nitis Monburinon <boonitis@gmail.com>
2018-07-27 10:14:08 +0900

    FIx

--- motion-detector-example.py
+++ motion-detector-example.py
@@ -9,10 +9,15 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, GPIO.PUD_DOWN)
 GPIO.setup(10, GPIO.OUT)
+# Initiate a counter
+i = 0
 
 # Main code goes below here
 try:
     while True:
+
+        # Print a counter
+        print(i)
 
 	# Detect motion via PIR Sensor
         detect = GPIO.input(4)
@@ -21,18 +26,19 @@ try:
         if(detect == 1):
             print("Motion Detected")
             # Blink LED
-            # GPIO.output(10,GPIO.HIGH)
-            # print("LED ON")
+            GPIO.output(10,GPIO.HIGH)
+            print("LED ON")
             # Wait 1 second
-            # time.sleep(2)
-            # GPIO.output(10,GPIO.LOW)
-            # print("LED OFF")
+            time.sleep(1)
+            GPIO.output(10,GPIO.LOW)
+            print("LED OFF")
             # Send data to server
-            # send_data.send("BOON")
+            send_data.send("BOON")
 
-        # Wait for further motion
+        # Wait a second before detecting further motion
         time.sleep(1)
-        print(".",end="")
+        # Increase a counter
+        i += 1
 
 except KeyboardInterrupt:
     GPIO.cleanup()
